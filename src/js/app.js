49ac2c08b33121e551753076d1c7adb2759192ea
sugarskill <fedbysandrine@gmail.com>
2022-10-08 01:17:36 +0200

    feat(cqfill): support container queries with polyfill

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,7 @@
-document.querySelector("html").classList.remove("no-js");
-document.querySelector("html").classList.add("js");
+// Support Test
+const supportsContainerQueries = "container" in document.documentElement.style;
+
+// Conditional Import
+if (!supportsContainerQueries) {
+  import("https://cdn.jsdelivr.net/npm/container-query-polyfill@0.2.4/dist/container-query-polyfill.modern.js");
+}
