067771bed0c405b918f7a8740be29d7a183be5c4
Sandrine <fedbysandrine@gmail.com>
2022-12-14 12:00:34 +1300

    feat: warning older browsers

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,1 @@
-const supportsContainerQueries="container"in document.documentElement.style;supportsContainerQueries||import("https://cdn.jsdelivr.net/npm/container-query-polyfill@0.2.4/dist/container-query-polyfill.modern.js");
+const supportsContainerQueries="container"in document.documentElement.style;
