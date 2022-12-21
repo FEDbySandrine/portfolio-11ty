ab5d667b223115dc6848d95ef7b32a5088de76f8
Sandrine <fedbysandrine@gmail.com>
2022-12-22 10:22:39 +1300

    clean: 11ty config

---
+++ config/shortcodes/index.js
@@ -0,0 +1,11 @@
+const img = require('./img');
+const imgResponsive = require('./imgResponsive');
+const svgIcon = require('./svgIcon');
+const svgImage = require('./svgImage');
+
+module.exports = {
+    img,
+    imgResponsive,
+    svgIcon,
+    svgImage
+};
