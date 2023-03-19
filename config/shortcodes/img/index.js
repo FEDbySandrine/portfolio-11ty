f118182e14deebf948ba5a1fd14143297fa423da
Sandrine <fedbysandrine@gmail.com>
2023-03-19 19:12:33 +1300

    perf: optimise non-responsive images shortcode

--- config/shortcodes/img/index.js
+++ config/shortcodes/img/index.js
@@ -4,7 +4,10 @@ const img = (filename, alt="", width="100%", height="100%", lazy=true, ext="jpg"
     if(lazy) { img_data +=" loading=\"lazy\""; }
     if(classname) { img_data +=" class=\"" + classname+ "\""; }
     return `
-        <img src="/assets/images/${filename}.${ext}" alt="${alt}" ${img_data} width="${width}" height="${height}">
+    <picture>
+        <source srcset="/assets/images/${filename}.webp, /assets/images/${filename}@2x.webp 2x" type="image/webp" />
+        <img src="/assets/images/${filename}.${ext}" srcset="/assets/images/${filename}@2x.${ext} 2x" alt="${alt}" ${img_data} width="${width}" height="${height}">
+    </picture>
     `;
 };
 module.exports = img;
